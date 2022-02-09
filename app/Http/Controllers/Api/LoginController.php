ba95339968502fb35421b5cfe659a950eb086f5b
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-02-09 04:30:33 -0400

    Login Api

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+      // Login true
+      // Login false
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V2\PostController;
 
 
-Route::apiResource('v2/posts', PostController::class);
+Route::apiResource('v2/posts', PostController::class)->only(['show', 'index'])->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, 'login']);
